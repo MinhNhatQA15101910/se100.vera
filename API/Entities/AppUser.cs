45a8101b1ab1290bdaf41e34ddb4c72e17075069
Do Nhat <dmnhat.pt@gmail.com>
2024-09-26 11:49:55 +0700

    Create initial data and add attributes to AppUser.

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,9 +1,24 @@
 namespace API.Entities;
 
+public enum Role
+{
+    Admin,
+    Artist,
+    User
+}
+
 public class AppUser
 {
     public int Id { get; set; }
     public required string Email { get; set; }
     public byte[] PasswordHashed { get; set; } = [];
     public byte[] PasswordSalt { get; set; } = [];
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Gender { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public string? About { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public Role Role { get; set; } = Role.User;
+    public List<Photo> Photos { get; set; } = [];
 }
