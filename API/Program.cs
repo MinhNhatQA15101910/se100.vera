bd6b3bc3b94176525da7ab438a92519b469fccf9
Do Nhat <dmnhat.pt@gmail.com>
2024-10-10 16:49:26 +0700

    Fix some small error.

--- API/Program.cs
+++ API/Program.cs
@@ -35,7 +35,7 @@ try
 catch (Exception ex)
 {
     var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "An eyrror occurred during migration");
+    logger.LogError(ex, "An error occurred during migration");
 }
 
 app.Run();
