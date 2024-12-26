8ac36bbc1ff820ed22e232cb5daf5e7d38ce532b
Do Nhat <dmnhat.pt@gmail.com>
2024-12-26 16:56:14 +0700

    Edit user seed data.

--- API/Program.cs
+++ API/Program.cs
@@ -33,9 +33,9 @@ try
     await Seed.SeedPhotos(context);
     await Seed.SeedUsers(context, userManager, roleManager);
     await Seed.SeedGenres(context);
-    await Seed.SeedSongs(context);
-    await Seed.SeedAlbums(context);
-    await Seed.SeedPlaylists(context);
+    // await Seed.SeedSongs(context);
+    // await Seed.SeedAlbums(context);
+    // await Seed.SeedPlaylists(context);
 }
 catch (Exception ex)
 {
