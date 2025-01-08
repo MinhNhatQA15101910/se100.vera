6fd76e2783295125ec8876e8095d217ec405fea6
JustVvu <21521682@gm.uit.edu.vn>
2025-01-08 22:04:15 +0700

    Done update playlist

--- API/Controllers/PlaylistsController.cs
+++ API/Controllers/PlaylistsController.cs
@@ -58,6 +58,31 @@ public class PlaylistsController(
       return mapper.Map<PlaylistDto>(playlist);
    }
 
+   [HttpPut("{id:int}")]
+   [Authorize(Roles = "Listener, Artist")]
+   public async Task<ActionResult<PlaylistDto>> UpdatePlaylist([FromForm] UpdatePlaylistDto updatePlaylistDto, int id)
+   {
+      if (updatePlaylistDto == null)
+      {
+         return BadRequest("Invalid playlist data.");
+      }
+
+      var playlist = await unitOfWork.PlaylistRepository.GetPlaylistByIdAsync(id);
+      if (playlist == null)
+      {
+         return NotFound("Playlist not found.");
+      }
+
+      var userId = User.GetUserId();
+      if (playlist.PublisherId != userId)
+      {
+         return Unauthorized("The playlist does not belong to you.");
+      }
+
+      mapper.Map(updatePlaylistDto, playlist);
+
+      return mapper.Map<PlaylistDto>(playlist);
+   }
 
    [HttpPut("add-song/{id:int}")]
    [Authorize(Roles = "Listener, Artist")]
