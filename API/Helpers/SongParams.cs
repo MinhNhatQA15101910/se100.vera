8cf901e52dc8c1cf7ba84d553439bb590adb9a31
JustVvu <21521682@gm.uit.edu.vn>
2025-01-08 19:30:02 +0700

    Merge with Nhat branch

--- API/Helpers/SongParams.cs
+++ API/Helpers/SongParams.cs
@@ -5,7 +5,6 @@ public class SongParams : PaginationParams
    public string? PublisherId { get; set; }
    public string? SongName { get; set; }
    public string? Artist { get; set; }
-   public string? PublisherId { get; set; }
    public string? OrderBy { get; set; } = "songName";
    public string? SortBy { get; set; } = "asc";
 }
